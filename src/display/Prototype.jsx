dd9069aecbb4e3a3a2a98d7e9b939904cc38824a
gjh <guyjohnhilton@gmail.com>
2026-03-07 00:56:41 +0000

    j

--- src/display/Prototype.jsx
+++ src/display/Prototype.jsx
@@ -1,67 +1,75 @@
-import { styled } from '../styled-system/jsx';
+import React from "react"
+import { styled } from "../styled-system/jsx"
 
-export const Item = ({ 
-	bg,
+// container - n rows on mobile / n columns on desktop
+const Item = styled("div", {
+  base: {
+    display: "grid",
+    gridTemplateColumns: { base: "1fr", md: "repeat(auto-fit, minmax(0, 1fr))" },
+  },
+})
+
+// row on mobile / column on desktop
+const Block = styled("div", {
+  base: {
+    bg: "gray.100"
+  },
+})
+
+export const BlockTitle = ({
+	title,
 	children
-}) => 
-<styled.div
-	bg={{
-        base: bg,
-        lg: "green.400"
-      }}
-    >
-      {children}
-	</styled.div>
+}) =>
+	<Block>
+		{title}
+		{children}
+	</Block>
+
+export const BlockCurrency = ({children}) =>
+	<Block>
+	currency
+	</Block>
+
+export const ItemUnit = () =>
+	<Item>
+		<BlockTitle title="unit item title"/>
+		<BlockCurrency />
+	</Item>
+
+export const ItemExtended = () =>
+	<Item>
+		<BlockTitle title="Extended item title"/>
+		<BlockCurrency />
+		<BlockCurrency />
+	</Item>
+
+export const ItemSubTotal = () =>
+	<Item>
+		<BlockTitle title="subtotal item title"/>
+		<BlockCurrency />
+	</Item>
+
+export const ItemTotal = () =>
+	<Item>
+		<BlockTitle title="Summa logo" />
+		<BlockCurrency />
+	</Item>
+
+export const HeaderEdit = () =>
+	<header>
+	edit buttons here
+	</header>
+
+export const ListOfItems = () =>
+	<section>
+		<ItemUnit />
+		<ItemExtended />
+		<ItemSubTotal />
+		<ItemTotal />
+	</section>
 
 export const ScreenMain = () =>
 	<main>
 		<HeaderEdit />
 		<ListOfItems/>
 	</main>
-	
-export const HeaderEdit = () =>
-	<header>
-	edit buttons here
-	</header>	
-	
-export const ListOfItems = () =>
-	<section>
-		<ItemUnit />
-	</section>
-	
-export const ItemUnit = () =>
-	<Item>
-	unit
-	</Item>	
-	
-export const ItemExtended = () =>
-	<Item>
-	extended
-	</Item>
-
-export const ItemSubTotal = () =>
-	<Item>
-	sub
-	</Item>
-	
-export const ItemTotal = () =>
-	<Item>
-	total
-	</Item>
-	
-export const Block = ({children}) =>
-	<div>
-	{children}
-	</div>
-
-export const BlockTitle = ({children}) =>
-	<Block>
-	title
-	{children}
-	</Block>
-	
-export const BlockCurrency = ({children}) =>
-	<Block>
-	currency
-	</Block>
-	
