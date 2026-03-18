6efb098a6bfdcf00bc1dd0b87d8fa4c8e2e642a5
gjh <guyjohnhilton@gmail.com>
2026-03-18 15:30:23 +0000

    merge

--- src/display/help/HelpAbout.tsx
+++ src/display/help/HelpAbout.tsx
@@ -9,22 +9,34 @@ const HelpLink = styled('a', {
   },
 })
 
+const HelpList = styled('ul', {
+  base: {
+    margin: 0,
+    paddingLeft: '1.25rem',
+    fontSize: '1rem',
+    lineHeight: 1.8,
+    color: '#333',
+  },
+})
+
 export function HelpAbout() {
   return (
     <HelpSection>
       <HelpHeading>About Summa</HelpHeading>
 
       <HelpPara>
-        If you find a bug or have a feature requestplease open an issue on the{' '}
+        If you find a bug or have a feature request, please open an issue on the{' '}
         <HelpLink href="https://github.com/gjhilton/Summa/issues">
           GitHub issues page
         </HelpLink>
-        Features currently under consideration are
-        - better printed output
-        - copy calculations to clipboard
-        - allow ine items to include an obolus
-        - (your request here)!
+        . Features currently under consideration include:
       </HelpPara>
+      <HelpList>
+        <li>better printed output</li>
+        <li>copy calculations to clipboard</li>
+        <li>allow line items to include an obolus</li>
+        <li>(your request here!)</li>
+      </HelpList>
     </HelpSection>
   )
 }
